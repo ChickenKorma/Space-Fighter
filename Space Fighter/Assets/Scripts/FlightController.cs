4a226159a6e1f5e3c0e1221dab8a0b64069939c1
Fraser Curry <fraserjcurry@gmail.com>
2022-05-16 00:09:06 +0100

    Reworked mouse control, updated asteroids and added crosshair

--- Space Fighter/Assets/Scripts/FlightController.cs
+++ Space Fighter/Assets/Scripts/FlightController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class FlightController : MonoBehaviour
 {
+    // Public variables not serialized
     public static FlightController instance;
 
-    [SerializeField] private float thrust, velocityDamping, maxVelocity, minVelocity, pitchSensitivity, yawSensitivity, rollSensitivity, rotationScale, rotationDamping;
+    [System.NonSerialized] public Vector3 mouseRelative;
 
-    public float velocity = 0.0f;
+    [System.NonSerialized] public float velocity;
+
+    // Private variables set in inspector
+    [SerializeField] private float pitchSensitivity, yawSensitivity, pitchYawScale, rollSensitivity, maxRotation, rotationDamping, deadZone;
+    [SerializeField] private float thrust, velocityDamping, minVelocity, maxVelocity;
+
+    // Private variables
+    private Vector3 screenCenter;
 
     private float targetVelocity = 0.0f;
-
-    private Vector3 targetRotation = new Vector3(0, 0, 0);
+    private float rotX, rotY, rotZ;
 
     private void Awake()
     {
@@ -31,27 +38,57 @@ public class FlightController : MonoBehaviour
     {
         // Lock cursor to game window and hide
         Cursor.lockState = CursorLockMode.Locked;
+
+        // Find screen size and set center
+        screenCenter = new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, 0.0f);
     }
 
     void Update()
     {
-        // Get rotation from player input and apply to transform
-        float rotX = -Input.GetAxis("Mouse Y") * pitchSensitivity * Time.deltaTime * rotationScale;
-        float rotY = Input.GetAxis("Mouse X") * yawSensitivity * Time.deltaTime * rotationScale;
-        float rotZ = -Input.GetAxis("Horizontal") * rollSensitivity * Time.deltaTime * rotationScale;
+        mouseRelative = Input.mousePosition - screenCenter;
+
+        float newRotX = 0.0f, newRotY = 0.0f;
+
+        // Apply a dead zone to the mouse position and clamp the rotation intensity
+        if(mouseRelative.y >= deadZone)
+        {
+            float rotationAmount = Mathf.Clamp(mouseRelative.y - deadZone, 0.0f, maxRotation);
+            newRotX = -rotationAmount * pitchSensitivity * Time.deltaTime * pitchYawScale;
+        }
+        else if(mouseRelative.y <= -deadZone)
+        {
+            float rotationAmount = Mathf.Clamp(mouseRelative.y + deadZone, -maxRotation, 0.0f);
+            newRotX = -rotationAmount * pitchSensitivity * Time.deltaTime * pitchYawScale;
+        }
+
+        if (mouseRelative.x >= deadZone)
+        {
+            float rotationAmount = Mathf.Clamp(mouseRelative.x - deadZone, 0.0f, maxRotation);
+            newRotY =  rotationAmount * yawSensitivity * Time.deltaTime * pitchYawScale;
+        }
+        else if (mouseRelative.x <= -deadZone)
+        {
+            float rotationAmount = Mathf.Clamp(mouseRelative.x - deadZone, -maxRotation, 0.0f);
+            newRotY = rotationAmount * yawSensitivity * Time.deltaTime * pitchYawScale;
+        }
+
+        // Get roll amount from keyboard input
+        float newRotZ = -Input.GetAxis("Horizontal") * rollSensitivity * Time.deltaTime;
+
+        // Smooth rotation values from previous frame and apply rotations
+        rotX = Mathf.Lerp(rotX, newRotX, rotationDamping);
+        rotY = Mathf.Lerp(rotY, newRotY, rotationDamping);
+        rotZ = Mathf.Lerp(rotZ, newRotZ, rotationDamping);
 
         Vector3 rotation = new Vector3(rotX, rotY, rotZ);
-        rotation = Vector3.Lerp(new Vector3(0, 0, 0), rotation, rotationDamping);
-
         transform.Rotate(rotation);
 
-
-        // Get thrust input from player and apply velocity to transform
+        // Get thrust amount from keyboard input and apply to target velocity
         targetVelocity += Input.GetAxis("Vertical") * thrust * Time.deltaTime;
         targetVelocity = Mathf.Clamp(targetVelocity, minVelocity, maxVelocity);
 
+        // Smooth velocity from previous frame and apply to position
         velocity = Mathf.Lerp(velocity, targetVelocity, velocityDamping);
-
         transform.position += velocity * transform.forward * Time.deltaTime;
     }
 }
--- Space Fighter/Assets/Scripts/UIManager.cs
+++ Space Fighter/Assets/Scripts/UIManager.cs
@@ -5,21 +5,21 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    // Private variables set in inspector
     [SerializeField] private Text velocityText;
 
     [SerializeField] private Slider throttleSlider;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Image crossHairs;
 
-    // Update is called once per frame
     void Update()
     {
+        // Update cockpit UI
         velocityText.text = "Velocity: " + FlightController.instance.velocity.ToString("0.00") + "m/s";
 
         throttleSlider.value = Input.GetAxis("Vertical");
+
+        // Update screen space UI
+        crossHairs.rectTransform.anchoredPosition = new Vector2(FlightController.instance.mouseRelative.x, FlightController.instance.mouseRelative.y);
     }
 }
